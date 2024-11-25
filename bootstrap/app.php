e641c4727d4e10d61e001047cc5edf9884925ae2
bethyhagos <bethyhag@gmail.com>
2024-11-25 11:51:17 +0300

    Add migration files and update configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,10 +7,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-<<<<<<< HEAD
-=======
-        api: __DIR__.'/../routes/api.php',
->>>>>>> 20ad7c550b36590391adfdc7803098ef289f1ab8
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
+
 Route::get('/Hello', function () {
     return view('welcome');
 });
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/hello', function () {
-    return 'Hello, Laravel!';
-});
->>>>>>> 20ad7c550b36590391adfdc7803098ef289f1ab8
