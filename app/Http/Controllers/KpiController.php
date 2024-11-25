728edc953c4a5ffe7db921fd91b83b6693f11dd9
bethyhagos <bethyhag@gmail.com>
2024-11-25 10:47:12 +0300

    Normalize line endings

---
+++ app/Http/Controllers/KpiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kpi;
+use App\Http\Requests\StoreKpiRequest;
+use App\Http\Requests\UpdateKpiRequest;
+
+class KpiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKpiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kpi $kpi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kpi $kpi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKpiRequest $request, Kpi $kpi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kpi $kpi)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/Hello', function () {
+    return view('welcome');
+});
